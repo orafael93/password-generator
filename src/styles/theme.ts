beb54323ff11c11920a8fd30d807a51fe40fb51c
Rafael Pereira <orafaeldev@gmail.com>
2023-06-12 18:10:59 -0300

    feat: adicao do arquivo do tema

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    grey: {
+      dark: '#24232C',
+      grey: '#817D92',
+      almostWhite: '#E6E5EA',
+      veryDark: '#18171F',
+    },
+    background: '#121118',
+    neonGreen: '#A4FFAF',
+    red: '#F64A4A',
+    orange: '#FB7C58',
+    yellow: '#F8CD65',
+  },
+};
