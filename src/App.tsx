8cce4412e8a682b92f0d7d01b3967c9d0a3c8168
Rafael Pereira <orafaeldev@gmail.com>
2023-06-26 18:13:16 -0300

    feat: adicao dos componentes no arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-function App() {
+import CharacterLength from './components/CharacterLength';
+import CharacterStrength from './components/CharacterStrength';
+import { ContentWrapper } from './components/ContentWrapper';
+import MainWrapper from './components/MainWrapper';
+import PasswordField from './components/PasswordField';
+import WrapperCheckboxes from './components/WrapperCheckboxes';
+import { GlobalProvider } from './context/GlobalProvider';
+import { AppName } from './styles/globalStyles';
 
+function App() {
   return (
-    <div />
-  )
+    <GlobalProvider>
+      <MainWrapper>
+        <AppName>Password Generator</AppName>
+        <PasswordField />
+        <ContentWrapper>
+          <CharacterLength />
+          <WrapperCheckboxes />
+          <CharacterStrength />
+        </ContentWrapper>
+      </MainWrapper>
+    </GlobalProvider>
+  );
 }
 
-export default App
+export default App;
