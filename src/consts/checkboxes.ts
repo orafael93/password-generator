56d8685ddbbd8a55e39f2e5df3e76e943ec4437b
Rafael Pereira <orafaeldev@gmail.com>
2023-07-25 18:18:11 -0300

    feat: adicao do arquivo de constantes dos checkboxes

---
+++ src/consts/checkboxes.ts
@@ -0,0 +1,18 @@
+export const checkboxes = [
+  {
+    id: 'uppercase',
+    label: 'Include Uppercase Letters',
+  },
+  {
+    id: 'lowercase',
+    label: 'Include LowerCase Letters',
+  },
+  {
+    id: 'numbers',
+    label: 'Include Numbers',
+  },
+  {
+    id: 'symbols',
+    label: 'Include Symbols',
+  },
+];
