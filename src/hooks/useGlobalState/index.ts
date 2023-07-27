34b41949e33ad8725f9fb6c962b0340b30bcc049
Rafael Pereira <orafaeldev@gmail.com>
2023-07-27 20:34:03 -0300

    feat: adicao do hook useGlobalState

---
+++ src/hooks/useGlobalState/index.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+
+import { GlobalContext } from '../../context/GlobalContext';
+
+export const useGlobalState = () => {
+  const globalContext = useContext(GlobalContext);
+
+  if (!globalContext) {
+    throw new Error('useGlobalState must be within a GlobalProvider');
+  }
+
+  return globalContext;
+};
