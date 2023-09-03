7ceeb21552a2bf67145c53cdd03882cd277b5c26
Rafael Pereira <orafaeldev@gmail.com>
2023-09-03 10:41:37 -0300

    feat: adicao do arquivo de estilos do componente Button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,37 @@
+import styled, { css } from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin-top: 32px;
+`;
+
+export const Button = styled.button`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+
+    background: ${theme.colors.neonGreen};
+    color: ${theme.colors.grey.dark};
+
+    text-transform: uppercase;
+    font-size: 18px;
+    font-weight: bold;
+
+    border: 2px solid transparent;
+
+    width: 100%;
+
+    cursor: pointer;
+
+    padding: 20px;
+
+    transition: 0.15s;
+
+    &:hover {
+      background: ${theme.colors.grey.dark};
+      color: ${theme.colors.neonGreen};
+      border-color: ${theme.colors.neonGreen};
+    }
+  `}
+`;
