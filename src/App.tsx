065e7ae5e7962da4a52f73cb02be214db85f5b40
Rafael Pereira <orafaeldev@gmail.com>
2025-11-29 21:31:38 -0300

    refactor: refactorar componente App e main

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import { Button } from './components/Button';
-import { MainWrapper } from './components/MainWrapper';
-import { PasswordField } from './components/PasswordField';
-import { GlobalProvider } from './context/GlobalProvider';
-import { ContentWrapper } from './components/ContentWrapper';
-import { CharacterLength } from './components/CharacterLength';
-import { CharacterStrength } from './components/CharacterStrength';
-import { CheckboxesWrapper } from './components/CheckboxesWrapper';
+import { Button } from '@/components/Button';
+import { MainWrapper } from '@/components/MainWrapper';
+import { PasswordField } from '@/components/PasswordField';
+import { GlobalProvider } from '@/context/GlobalProvider';
+import { ContentWrapper } from '@/components/ContentWrapper';
+import { CharacterLength } from '@/components/CharacterLength';
+import { CharacterStrength } from '@/components/CharacterStrength';
+import { CheckboxesWrapper } from '@/components/CheckboxesWrapper';
 
-import { AppName } from './styles/globalStyles';
+import { AppName } from '@/styles/globalStyles';
 
-function App() {
-  return (
-    <GlobalProvider>
-      <MainWrapper>
-        <AppName>Password Generator</AppName>
-        <PasswordField />
-        <ContentWrapper>
-          <CharacterLength />
-          <CheckboxesWrapper />
-          <CharacterStrength />
-          <Button>Generate</Button>
-        </ContentWrapper>
-      </MainWrapper>
-    </GlobalProvider>
-  );
-}
-
-export default App;
+export const App = () => (
+  <GlobalProvider>
+    <MainWrapper>
+      <AppName>Password Generator</AppName>
+      <PasswordField />
+      <ContentWrapper>
+        <CharacterLength />
+        <CheckboxesWrapper />
+        <CharacterStrength />
+        <Button>Generate</Button>
+      </ContentWrapper>
+    </MainWrapper>
+  </GlobalProvider>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App.tsx';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme.ts';
-import { GlobalStyles } from './styles/globalStyles.ts';
+
+import { App } from '@/App.tsx';
+import { theme } from '@/styles/theme';
+import { GlobalStyles } from '@/styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <>
+      <React.Fragment>
         <GlobalStyles />
         <App />
-      </>
+      </React.Fragment>
     </ThemeProvider>
   </React.StrictMode>,
 );
